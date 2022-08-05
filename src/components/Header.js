2681c1f9dbe5c0ee8e4bbc5c27c7cd3a34da26f8
joaocorner <joaoeduardocorner@gmail.com>
2022-08-05 11:39:59 -0300

    finishing CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const Header = ({ title }) => {
     return (
         <header>
-             <h1 style={headingStyle}>{title}</h1>{/*single curly braces in headingStyle*/}
+             <h1>{title}</h1>
         </header>
     )
 }
@@ -16,9 +16,10 @@ Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
 
-const headingStyle = {
-    color: 'red',
-    backgroundColor: 'black'
-}
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
 
 export default Header
