7ecd72212558e17db1adf30e352f7a40d9b01ff2
joaocorner <joaoeduardocorner@gmail.com>
2022-08-05 10:02:51 -0300

    Use of component props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+//import React from "react"; // if you are using class instead of function
+
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="App">
-     <h1>Hello World</h1>
+    <div className="container">
+      <Header title='Hi' />
     </div>
   );
 }
+
+// class App extends React.Component { // if you are using class instead of function
+//   render() {
+//     return <h1>Hello World</h1>
+//   }
+// }
 
 export default App;
