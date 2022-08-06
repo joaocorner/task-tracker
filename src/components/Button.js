103dff57ab88f341ecb38468021664031bc830fc
joaocorner <joaoeduardocorner@gmail.com>
2022-08-06 20:39:10 -0300

    finishing style button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const Button = ({ color, text }) => { // color and text are props
     return (
         <button
@@ -6,6 +8,15 @@ const Button = ({ color, text }) => { // color and text are props
             {text}
         </button>
     )
+}
+
+Button.defaultProps = {
+    color: 'steelblue',
+}
+
+Button.propTypes = {
+    color: PropTypes.string,
+    text: PropTypes.string
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,7 @@ const Header = ({ title }) => {
     return (
         <header className='header'>
              <h1>{title}</h1>
-             <Button color='green' text='hi'/>
-             <Button color='orange' text='hi'/>
-             <Button color='red' text='hi'/>
+             <Button color='green' text='Add'/>
         </header>
     )
 }
