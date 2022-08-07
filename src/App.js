57369589d6d716de5bad489bfff6a0ddeb9094d1
joaocorner <joaoeduardocorner@gmail.com>
2022-08-07 20:27:35 -0300

    changing array tasks so it can be global

--- src/App.js
+++ src/App.js
@@ -2,12 +2,37 @@
 
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from 'react'; //bring this from Tasks
 
-function App() {
+const App = () => {
+
+  const [tasks, setTasks] = useState(// useState is a hook that allows you to manage the state of a component, in the right side of the equal sign you pass the initial state of the component. If you want to change the state of the component you have to use the setTasks function.
+    [
+      {
+        id: 1,
+        text: 'Do the laundry',
+        day: 'Feb 5th at 5:00pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Do the dishes',
+        day: 'Feb 7th at 1:00pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Make breakfast',
+        day: 'Feb 8th at 8:30am',
+        reminder: false,
+      }
+    ]
+  )
+
   return (
     <div className="container">
       <Header />{/* if you remove the title(title:"blablabla"), it will show the default title */}
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,34 +1,12 @@
-import { useState } from 'react';
 
-const Tasks = () => {
-    const [tasks, setTasks] = useState(// useState is a hook that allows you to manage the state of a component, in the right side of the equal sign you pass the initial state of the component. If you want to change the state of the component you have to use the setTasks function.
-        [
-            {
-                id: 1,
-                text: 'Do the laundry',
-                day: 'Feb 5th at 5:00pm',
-                reminder: true,
-            },
-            {
-                id: 2,
-                text: 'Do the dishes',
-                day: 'Feb 7th at 1:00pm',
-                reminder: true,
-            },
-            {
-                id: 3,
-                text: 'Make breakfast',
-                day: 'Feb 8th at 8:30am',
-                reminder: false,
-            }
-        ]
-    )
+
+const Tasks = (props) => {
 
     return (
         //don't use tasks.push() in the render function, use the setTasks function instead to insert a new task in the array. State is imutable, you can't change the state directly.
-               
+
         <>
-            {tasks.map((task) => (
+            {props.tasks.map((task) => (
                 <h3 key={task.id}>{task.text}</h3>// key is used to identify each element in an array. It's used in parent components.
             ))}
         </>
