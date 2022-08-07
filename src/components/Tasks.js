966bf7eaddde8e8b31c4ea100a1b0b37641658ef
joaocorner <joaoeduardocorner@gmail.com>
2022-08-07 20:16:25 -0300

    adding some obs

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const Tasks = () => {
-    const [tasks, setTasks] = useState(// useState is a hook that allows you to manage the state of a component
+    const [tasks, setTasks] = useState(// useState is a hook that allows you to manage the state of a component, in the right side of the equal sign you pass the initial state of the component. If you want to change the state of the component you have to use the setTasks function.
         [
             {
                 id: 1,
@@ -25,6 +25,8 @@ const Tasks = () => {
     )
 
     return (
+        //don't use tasks.push() in the render function, use the setTasks function instead to insert a new task in the array. State is imutable, you can't change the state directly.
+               
         <>
             {tasks.map((task) => (
                 <h3 key={task.id}>{task.text}</h3>// key is used to identify each element in an array. It's used in parent components.
