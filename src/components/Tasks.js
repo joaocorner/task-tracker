fa9e557e8240b87f5ed9724597e730d09430318b
joaocorner <joaoeduardocorner@gmail.com>
2022-08-07 20:48:28 -0300

    adding individual component task and formatting

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,16 +1,16 @@
+import Task from "./Task"
 
-
-const Tasks = (props) => {
+const Tasks = ({ tasks }) => {
 
     return (
         //don't use tasks.push() in the render function, use the setTasks function instead to insert a new task in the array. State is imutable, you can't change the state directly.
 
         <>
-            {props.tasks.map((task) => (
-                <h3 key={task.id}>{task.text}</h3>// key is used to identify each element in an array. It's used in parent components.
+            {tasks.map((task) => (
+                <Task key={task.id} task={task}/>// key is used to identify each element in an array. It's used in parent components.
             ))}
-        </>
-    )
+                </>
+            )
 }
 
-export default Tasks
+            export default Tasks
