5f17d0021e57ca24e8faa3b25663ffb5de6c5567
joaocorner <joaoeduardocorner@gmail.com>
2022-08-09 15:58:09 -0300

    adding the add task component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from 'react';
 
 const App = () => {
@@ -40,11 +41,12 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <AddTask />
       {tasks.length > 0 ?
         <Tasks
-          tasks={tasks}
-          onDelete={deleteTask}
-          onToggle={toggleReminder}
+        tasks={tasks}
+        onDelete={deleteTask}
+        onToggle={toggleReminder}
         />
         :
         ' No tasks to display'}
